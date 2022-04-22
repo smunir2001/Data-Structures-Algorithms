e274d8bf089f95f4a1510964ceef8c9e0db1dd4e
Sami Munir <smunir2001@gmail.com>
2022-04-22 16:31:09 -0400

    complete StaticArrayDS.java constructor

--- Java Programming/StaticArray/StaticArrayDS.java
+++ Java Programming/StaticArray/StaticArrayDS.java
@@ -4,5 +4,16 @@
 */
 
 public class StaticArrayDS {
-    //
+    int[] mainArray = null;
+    int tailPointer = -1;
+
+    public StaticArrayDS(int inputLength) {
+        if (inputLength <= 0) {
+            System.out.println("\nERROR: Cannot initialize mainArray[] with inputLength = " + inputLength + ".");
+        } else {
+            System.out.println("\nInitializing mainArray[] to length = " + inputLength + ".");
+            mainArray = new int[inputLength];
+            tailPointer++;
+        }
+    }
 }
