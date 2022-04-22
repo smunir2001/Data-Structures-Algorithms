fac23c64983085a3d4c522bfaaffdadd8714738e
Sami Munir <smunir2001@gmail.com>
2022-04-22 17:50:16 -0400

    updated testing for insertAtHead()

--- Java Programming/StaticArray/Main.java
+++ Java Programming/StaticArray/Main.java
@@ -11,13 +11,31 @@ public class Main {
         System.out.println("\narr0 -->");
         StaticArrayDS arr0 = new StaticArrayDS(0);
         arr0.printArray();
+        arr0.insertAtHead(3); // this will report an ERROR
+        arr0.printArray();
 
         System.out.println("\narr1 -->");
         StaticArrayDS arr1 = new StaticArrayDS(1);
         arr1.printArray();
+        arr1.insertAtHead(3);
+        arr1.printArray();
+        arr1.insertAtHead(22); // this will report an ERROR
+        arr1.printArray();
 
         System.out.println("\narr2 -->");
         StaticArrayDS arr2 = new StaticArrayDS(5);
+        arr2.printArray();
+        arr2.insertAtHead(3);
+        arr2.printArray();
+        arr2.insertAtHead(22);
+        arr2.printArray();
+        arr2.insertAtHead(7);
+        arr2.printArray();
+        arr2.insertAtHead(81);
+        arr2.printArray();
+        arr2.insertAtHead(146);
+        arr2.printArray();
+        arr2.insertAtHead(100); // this will report an ERROR
         arr2.printArray();
     }
 }
