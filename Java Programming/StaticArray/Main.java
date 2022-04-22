43a986a0f90e4171e2246edc36f8410c93c410f9
Sami Munir <smunir2001@gmail.com>
2022-04-22 16:49:41 -0400

    Main() test method implementation

--- Java Programming/StaticArray/Main.java
+++ Java Programming/StaticArray/Main.java
@@ -2,10 +2,22 @@
     Main.java
     - Testing class/program for StaticArrayDS.java
 */
-package GitHub-main.Java Programming.StaticArray;
 
 public class Main {
     public static void main(String[] args) {
-        //
+        System.out.println("StaticArrayDS.java");
+        System.out.println("------------------");
+        
+        System.out.println("\narr0 -->");
+        StaticArrayDS arr0 = new StaticArrayDS(0);
+        arr0.printArray();
+
+        System.out.println("\narr1 -->");
+        StaticArrayDS arr1 = new StaticArrayDS(1);
+        arr1.printArray();
+
+        System.out.println("\narr2 -->");
+        StaticArrayDS arr2 = new StaticArrayDS(5);
+        arr2.printArray();
     }
 }
