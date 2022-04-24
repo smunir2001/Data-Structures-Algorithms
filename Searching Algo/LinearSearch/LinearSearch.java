d547313db27f26e16ba157ff01adee13b70cfb36
Sami Munir <smunir2001@gmail.com>
2022-04-24 11:24:23 -0400

    adding LinearSearch.java

---
+++ Searching Algo/LinearSearch/LinearSearch.java
@@ -0,0 +1,21 @@
+/*
+    LinearSearch.java
+    - Linear search algorithm on arrays.
+*/
+
+public class LinearSearch {
+    private static void linearSearch(int[] inputArray, int target) {
+        for (int i = 0; i < inputArray.length; i++) {
+            if (inputArray[i] == target) {
+                System.out.println("\nSUCCESS: linearSearch() found " + target + " in inputArray[].");
+                return;
+            }
+        }
+        System.out.println("\nERROR: linearSearch() did not find " + target + " in inputArray[].");
+    }
+    public static void main(String[] args) {
+        int[] array1 = {3, 22, 7, 81, 146, -59};
+        linearSearch(array1, 81);
+        linearSearch(array1, 53);
+    }
+}
