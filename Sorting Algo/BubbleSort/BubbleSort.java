793f5525009b4ec55c780e1f6337779e0e7fd356
Sami Munir <smunir2001@gmail.com>
2022-04-24 13:13:36 -0400

    bubbleSort.java added

---
+++ Sorting Algo/BubbleSort/BubbleSort.java
@@ -0,0 +1,34 @@
+/*
+    BubbleSort.java
+    - Bubble sort algorithm on arrays.
+*/
+import java.util.Arrays;
+public class BubbleSort {
+    private static void bubbleSort(int[] inputArray) {
+        System.out.println("\nSUCCESS: bubbleSort()...");
+        for (int i = inputArray.length - 1; i > 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (inputArray[j] > inputArray[j + 1]) {
+                    swapValues(inputArray, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private static void swapValues(int[] inputArray, int index1, int index2) {
+        int temp = inputArray[index1];
+        inputArray[index1] = inputArray[index2];
+        inputArray[index2] = temp;
+    }
+    public static void main(String[] args) {
+        int[] array1 = {3, 22, 7, 81, 146, 59};
+        System.out.println("array1[] --> " + Arrays.toString(array1));
+        bubbleSort(array1);
+        System.out.println("bubbleSort(array1) --> " + Arrays.toString(array1));
+
+        int[] array2 = {3, 7, 22};
+        System.out.println("array2[] --> " + Arrays.toString(array2));
+        bubbleSort(array2);
+        System.out.println("bubbleSort(array2) --> " + Arrays.toString(array2));
+    }
+}
---
+++ Sorting Algo/BubbleSort/BubbleSortAlgo.md
@@ -0,0 +1,10 @@
+# Bubble Sort (Java Implementation)
+## Overview
+* ### The bubble sort algorithm bubbles up pairs of two adjacent elements and swaps them depending on ascending/descending order.
+
+## *bubbleSort()*
+
+## *swapValues()*
+
+## Time complexity
+* ### Quadratic-time complexity O(n^2)
